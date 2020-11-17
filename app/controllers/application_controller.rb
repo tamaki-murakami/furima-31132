268bc7387b3109dc4eaf07ea92377bd7f2a4d4a0
tamaki-murakami <72539750+tamaki-murakami@users.noreply.github.com>
2020-11-17 17:55:11 +0900

    ログアウト機能実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
-
+  
   private
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nick_name, :family_name, :last_name, :family_kana, :last_kana, :birthday])
