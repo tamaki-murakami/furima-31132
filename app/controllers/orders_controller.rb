c9593284bc2e0d0f762a6c88e056ef8d04c8ebcd
tamaki-murakami <72539750+tamaki-murakami@users.noreply.github.com>
2020-12-07 16:24:21 +0900

    テストコード記述

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,11 +1,17 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_item
+  before_action :move_to_root
+
   def new
-    # @model = Model.new
+    @order_form = OrderForm.new
   end
 
   def create
-    @model = Model.new(model_params)
-    if @model.save
+    @order_form = OrderForm.new(order_form_params)
+    if @order_form.valid?
+      pay_item
+      @order_form.save
       redirect_to root_path
     else
       render :new
@@ -14,7 +20,24 @@ class OrdersController < ApplicationController
 
   private
 
-  def model_params
-    params.require(:model).permit(:column).merge(user_id: current_user.id)
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
+  def move_to_root
+    redirect_to root_path if current_user.id == @item.user_id || @item.order
+  end
+
+  def order_form_params
+    paparams.require(:order_form).permit(:post, :prefecture_id, :town, :address, :building, :tel).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV['PAYJP_SECRET_KEY']
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: order_form_params[:token],
+      currency: 'jpy'
+    )
   end
 end
